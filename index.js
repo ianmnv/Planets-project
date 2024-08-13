a291672fa104cf249dcdd03b393820c4af293ded
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-08-12 21:36:20 -0600

    Finding habitable planets

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitable(planet) {
+  return (
+    planet.koi_disposition === "CONFIRMED" &&
+    planet.koi_prad < 1.6 &&
+    planet.koi_insol < 1.11 &&
+    planet.koi_insol > 0.36
+  );
+}
 
 fs.createReadStream("kepler_data.csv")
   .pipe(
@@ -10,9 +19,13 @@ fs.createReadStream("kepler_data.csv")
       columns: true,
     })
   )
-  .on("data", (data) => results.push(data))
+  .on("data", (data) => {
+    if (isHabitable(data)) {
+      habitablePlanets.push(data);
+    }
+  })
   .on("error", (error) => console.log(error))
   .on("end", () => {
-    console.log(results);
+    console.log(`${habitablePlanets.length} habitable planets`);
     console.log("done with stream");
   });
