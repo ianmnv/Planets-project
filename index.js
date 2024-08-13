65d70ad84dfef87be11a5224327c10dd394b0da5
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-08-12 21:02:45 -0600

    Parsing buffer objects into JS objects using csv-parse package

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const fs = require("fs");
 const results = [];
 
 fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => results.push(data))
   .on("error", (error) => console.log(error))
   .on("end", () => {
     console.log(results);
     console.log("done with stream");
   });
-// parse();
