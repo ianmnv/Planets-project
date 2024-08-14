f2d3ca918bb58b7595d139a38e809c3639615aa4
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-08-13 18:52:53 -0600

    Start routing

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const { parse } = require("csv-parse");
 const fs = require("fs");
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "application/json" });
-  res.end(JSON.stringify({ id: 1, name: "Ian" }));
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  if (req.url === "/friends") {
+    // res.writeHead(200, { "Content-type": "application/json" });
+    res.statusCode = 200;
+    res.setHeader("Content-type", "application/json");
+    res.end(JSON.stringify({ id: 1, name: "Ian" }));
+  } else if (req.url === "/messages") {
+    res.setHeader("Content-type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello from the server</li>");
+    res.write("<li>Hello from the server again</li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(3000, () => {
