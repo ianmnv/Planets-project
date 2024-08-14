dbdff7785dc2f787c476fb40a1ba2bc946637615
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-08-13 20:45:12 -0600

    Using methods from req.method

--- index.js
+++ index.js
@@ -2,21 +2,36 @@ const { parse } = require("csv-parse");
 const fs = require("fs");
 const http = require("http");
 
+const friends = [
+  { id: 0, name: "Erick" },
+  { id: 1, name: "Ian" },
+  { id: 2, name: "Cessar" },
+];
+
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-  if (req.url === "/friends") {
+  const items = req.url.split("/");
+  console.log(items);
+
+  if (req.method === "GET" && items[1] === "friends") {
     // res.writeHead(200, { "Content-type": "application/json" });
     res.statusCode = 200;
     res.setHeader("Content-type", "application/json");
-    res.end(JSON.stringify({ id: 1, name: "Ian" }));
-  } else if (req.url === "/messages") {
+    res.end(JSON.stringify(friends));
+  } else if (req.method === "POST" && items[1] === "friends") {
+    req.on("data", (data) => {
+      const friend = data.toString();
+      console.log("data:", friend);
+      friends.push(JSON.parse(friend));
+    });
+    req.pipe(res);
+  } else if (req.method === "GET" && items[1] === "messages") {
     res.setHeader("Content-type", "text/html");
     res.write("<html>");
     res.write("<body>");
     res.write("<ul>");
     res.write("<li>Hello from the server</li>");
-    res.write("<li>Hello from the server again</li>");
     res.write("</ul>");
     res.write("</body>");
     res.write("</html>");
